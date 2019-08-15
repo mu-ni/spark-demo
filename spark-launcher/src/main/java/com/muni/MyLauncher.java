63c161d9f0bc9a25fd948baea3307395ff137212
muni <muni@4paradigm.com>
2019-08-15 14:35:08 +0800

    add logs

--- spark-launcher/src/main/java/com/muni/MyLauncher.java
+++ spark-launcher/src/main/java/com/muni/MyLauncher.java
@@ -5,30 +5,34 @@ import org.apache.spark.launcher.SparkLauncher;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by muni on 2019/8/14
  */
 public class MyLauncher {
     public static void main(String[] args) {
-
-        // clear log
-        String errPath = MyLauncher.class.getClassLoader().getResource("spark-err.log").getPath();
-        String outputPath = MyLauncher.class.getClassLoader().getResource("spark-output.log").getPath();
-        String logFile = MyLauncher.class.getClassLoader().getResource("test.txt").getPath();
-
-        SparkLauncher launcher = new SparkLauncher()
-                .addSparkArg("--conf", "spark.driver.cores=1")
-                .setAppName("Test Spark")
-                .setMaster("local")
-                .setSparkHome("/Users/muni/Downloads/spark-2.4.0-bin-hadoop2.7")
-                .setMainClass("com.muni.SparkMain")
-                .setAppResource("/Users/muni/IdeaProjects/spark-demo/spark-main/target/spark-main-1.0-SNAPSHOT.jar")
-                .redirectError(new File(errPath))
-                .redirectOutput(new File(outputPath))
-                .addAppArgs(logFile);
+        String errFile = System.getProperty("user.dir") + "/logs/spark-err.log";
+        String outputFile = System.getProperty("user.dir") + "/logs/spark-output.log";
+        String logFile = MyLauncher.class.getClassLoader().getResource("test.txt").getFile();
 
         try {
+            // clear log
+            new PrintWriter(errFile).close();
+            new PrintWriter(outputFile).close();
+
+            SparkLauncher launcher = new SparkLauncher()
+                    .addSparkArg("--conf", "spark.driver.cores=1")
+                    .setAppName("Test Spark")
+                    .setMaster("local")
+                    .setSparkHome("/Users/muni/Downloads/spark-2.4.0-bin-hadoop2.7")
+                    .setMainClass("com.muni.SparkMain")
+                    .setAppResource("/Users/muni/IdeaProjects/spark-demo/spark-main/target/spark-main-1.0-SNAPSHOT.jar")
+                    .redirectError(new File(errFile))
+                    .redirectOutput(new File(outputFile))
+                    .addAppArgs(logFile);
+
+
             SparkAppHandle handler = launcher.startApplication(new SparkAppHandle.Listener() {
                 @Override
                 public void stateChanged(SparkAppHandle sparkAppHandle) {
@@ -45,9 +49,7 @@ public class MyLauncher {
                 System.out.println(handler.getAppId() + " -> " + handler.getState());
                 Thread.sleep(2000);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
